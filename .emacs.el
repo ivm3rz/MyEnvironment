8ccda174be109169783114e3e1eef029e1f5a636
ivmacs <igor.merzlyakov@gmail.com>
2021-08-16 16:28:39 +0300

    Emacs: fixed winum mode initalization

--- .emacs.el
+++ .emacs.el
@@ -1056,25 +1056,23 @@ compilation."
 ;; meta-(# key)
 (use-package winum
   :ensure t
-  :init
-  (defvar winum-keymap
-        (let ((map (make-sparse-keymap)))
-          (define-key map (kbd "M-0") 'winum-select-window-0-or-10)
-          (define-key map (kbd "M-1") 'winum-select-window-1)
-          (define-key map (kbd "M-2") 'winum-select-window-2)
-          (define-key map (kbd "M-3") 'winum-select-window-3)
-          (define-key map (kbd "M-4") 'winum-select-window-4)
-          (define-key map (kbd "M-5") 'winum-select-window-5)
-          (define-key map (kbd "M-6") 'winum-select-window-6)
-          (define-key map (kbd "M-7") 'winum-select-window-7)
-          (define-key map (kbd "M-8") 'winum-select-window-8)
-          map))
   :config
   (eval-when-compile
     ;; Silence missing function warnings
     (declare-function winum-mode "winum.el"))
   (setq winum-scope 'frame-local)
-  (winum-mode t)
+  (progn
+    (define-key winum-keymap (kbd "M-0") 'winum-select-window-0-or-10)
+    (define-key winum-keymap (kbd "M-1") 'winum-select-window-1)
+    (define-key winum-keymap (kbd "M-2") 'winum-select-window-2)
+    (define-key winum-keymap (kbd "M-3") 'winum-select-window-3)
+    (define-key winum-keymap (kbd "M-4") 'winum-select-window-4)
+    (define-key winum-keymap (kbd "M-5") 'winum-select-window-5)
+    (define-key winum-keymap (kbd "M-6") 'winum-select-window-6)
+    (define-key winum-keymap (kbd "M-7") 'winum-select-window-7)
+    (define-key winum-keymap (kbd "M-8") 'winum-select-window-8)
+    (define-key winum-keymap (kbd "M-9") 'winum-select-window-9)
+    (winum-mode t))
   )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
